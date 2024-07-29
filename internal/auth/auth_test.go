935322c660d1f52d5696f146e73effe49f402869
nerd-sandeep <sandeep.singh@gmail.com>
2024-07-29 08:14:37 +0530

    Changes to run tests in CI and formatted authtests file

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,42 +1,41 @@
 package auth
 
 import (
-    "reflect"
-    "testing"
-	"net/http"
 	"fmt"
-
+	"net/http"
+	"reflect"
+	"testing"
 )
 
 func TestGetAPIKey(t *testing.T) {
 	headers := http.Header{}
-	headers.Set("Authorization","ApiKey ValidKey")
+	headers.Set("Authorization", "ApiKey ValidKey")
 	apiKey, err := GetAPIKey(headers)
-    got := apiKey
-    want := "ValidKey"
+	got := apiKey
+	want := "ValidKey"
 	if err != nil {
 		fmt.Println("Error:", err)
 	} else {
 		fmt.Println("API Key:", apiKey)
 	}
 
-    if !reflect.DeepEqual(want, got) {
-         t.Fatalf("expected: %v, got: %v", want, got)
-    }
+	if !reflect.DeepEqual(want, got) {
+		t.Fatalf("expected: %v, got: %v", want, got)
+	}
 
-    // headers1 := http.Header{}
+	// headers1 := http.Header{}
 	// headers1.Set("Authorization","ApiKey ValidKey1")
 	// apiKey1, err1 := GetAPIKey(headers1)
-    // got1 := apiKey1
-    // want1 := "ValidKey1"
+	// got1 := apiKey1
+	// want1 := "ValidKey1"
 	// if err1 != nil {
 	// 	fmt.Println("Error:", err1)
 	// } else {
 	// 	fmt.Println("API Key:", apiKey1)
 	// }
 
-    // if !reflect.DeepEqual(want1, got1) {
-    //      t.Fatalf("expected: %v, got: %v", want1, got1)
-    // }
+	// if !reflect.DeepEqual(want1, got1) {
+	//      t.Fatalf("expected: %v, got: %v", want1, got1)
+	// }
 
 }
